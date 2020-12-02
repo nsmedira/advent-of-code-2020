a0a6cdbd756a40cac6e99819377652e9d5a85e26
Nick Smedira <nsmedira@me.com>
2020-12-02 13:55:09 -0500

    finish part 1

--- 02/part-1.py
+++ 02/part-1.py
@@ -1,3 +1,29 @@
 from parse import parse
 
-print(parse())
+if __name__ == "__main__":
+
+    t = parse()
+    
+    number_valid = 0
+
+    # convert each entry in the list into a list
+    for i in range(len(t)):
+
+        t[i] = t[i].split()
+
+        # t[i][0] will now be the range. split with '-'
+        # t[i][1] will be the character. strip ':'
+        # leave t[i][2] unmodified
+        t[i] = [t[i][0].split('-'), t[i][1].strip(':'), t[i][2]]
+
+        # count how many times the letter appears
+        count = 0
+        for j in list(t[i][2]):
+            if j == t[i][1]:
+                count += 1
+        
+        # does it fit policy?
+        if count >= int(t[i][0][0]) and count <= int(t[i][0][1]):
+            number_valid += 1
+
+    print(number_valid)
