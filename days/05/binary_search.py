14c15115f27eaa69f18dce26c04aabf8a64dd781
Nick Smedira <nsmedira@me.com>
2020-12-06 09:21:02 -0500

    modularize day 5

--- days/05/binary_search.py
+++ days/05/binary_search.py
@@ -13,3 +13,7 @@ def binary_search(string, specify_lower, length):
             r = [r[0] + ceil(dif), r[1]]
 
     return r[0]
+
+def get_row_and_seat(string):
+
+    return binary_search(string[:7], 'F', 128), binary_search(string[7:], 'L', 8)
--- days/05/part-2.py
+++ days/05/part-2.py
@@ -1,5 +1,5 @@
 from parse import parse
-from binary_search import binary_search
+from binary_search import get_row_and_seat
 
 if __name__ == '__main__':
 
@@ -8,16 +8,14 @@ if __name__ == '__main__':
 
     for boarding_pass in t:
 
-        r = binary_search(boarding_pass[:7], 'F', 128)
-
-        s = binary_search(boarding_pass[7:], 'L', 8)
+        r, s = get_row_and_seat(boarding_pass)
 
         ids += [r * 8 + s]
 
     ids.sort()
 
     for i in range(len(ids)):
-        
+
         if i == 0:
             continue
 
